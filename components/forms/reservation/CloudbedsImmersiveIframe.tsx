5261f5f670ba36c38c169709212472f0dd66f490
Adriana Rose <adriana.rose.dh@gmail.com>
2025-05-03 18:38:57 -0700

    removed sandbox

--- components/forms/reservation/CloudbedsImmersiveIframe.tsx
+++ components/forms/reservation/CloudbedsImmersiveIframe.tsx
@@ -9,7 +9,6 @@ export function CloudbedsImmersiveIframe({ className }: Props) {
     <iframe
       src="/cloudbeds-immersive.html"
       title="Casa Olivea Booking"
-      sandbox="allow-scripts allow-same-origin allow-forms allow-popups allow-top-navigation-by-user-activation"
       className={className ?? "w-full h-full border-0"}
     />
   );
