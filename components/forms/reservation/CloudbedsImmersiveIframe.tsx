7097c9af5ab28945d86a7fdab51eb46118cd8edd
Adriana Rose <adriana.rose.dh@gmail.com>
2025-05-03 18:15:40 -0700

    attempt again

--- components/forms/reservation/CloudbedsImmersiveIframe.tsx
+++ components/forms/reservation/CloudbedsImmersiveIframe.tsx
@@ -1,11 +1,9 @@
 // components/forms/reservation/CloudbedsImmersiveIframe.tsx
 "use client";
-
 export function CloudbedsImmersiveIframe() {
   return (
     <iframe
       src="/cloudbeds-immersive.html"
-      title="Hotel booking"
       className="w-full h-full border-0"
       sandbox="allow-scripts allow-same-origin allow-forms allow-popups"
     />
