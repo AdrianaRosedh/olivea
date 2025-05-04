45661d2327f6db2010e13b48cdbca304ad362ecf
Adriana Rose <adriana.rose.dh@gmail.com>
2025-05-03 17:59:08 -0700

    csp try again

--- components/forms/reservation/CloudbedsInlineIframe.tsx
+++ components/forms/reservation/CloudbedsInlineIframe.tsx
@@ -1,9 +1,10 @@
+// components/forms/reservation/CloudbedsInlineIframe.tsx
 "use client";
 
 export function CloudbedsInlineIframe() {
   return (
     <iframe
-      src="https://hotels.cloudbeds.com/widget/load/pkwNrX/inline"
+      src="/cloudbeds-inline.html"
       className="w-full h-full border-0"
       sandbox="allow-scripts allow-same-origin allow-forms"
     />
