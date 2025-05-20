f3dd64ba087bf99d5cb630588f000e4c484d54c8
Adriana Rose <adriana.rose.dh@gmail.com>
2025-05-19 20:19:17 -0700

    returned the white box

--- components/forms/reservation/TockWidget.tsx
+++ components/forms/reservation/TockWidget.tsx
@@ -49,16 +49,20 @@ export default function TockWidget() {
         }
       `}</style>
 
-      <div
-        id="Tock_widget_container"
-        data-tock-display-mode="Inline"
-        data-tock-widget="data-tock-offering"
-        data-tock-offering-id="528232"
-        data-tock-color-mode="White"
-        data-tock-locale="es-mx"
-        data-tock-timezone="America/Tijuana"
-        style={{ minHeight: "400px", width: "100%" }}
-      />
+    <div className="flex-1 flex justify-center items-start p-6 overflow-hidden">
+      <div className="p-6 bg-white rounded-xl w-full max-w-lg overflow-hidden relative">
+        <div
+          id="Tock_widget_container"
+          data-tock-display-mode="Inline"
+          data-tock-widget="data-tock-offering"
+          data-tock-offering-id="528232"
+          data-tock-color-mode="White"
+          data-tock-locale="es-mx"
+          data-tock-timezone="America/Tijuana"
+          style={{ width: "100%", minHeight: "400px", overflow: "auto" }}
+        />
+      </div>
+    </div>
     </>
   );
 }
