3bf11b3c07e04cdcb87cc1425315229d65e74d9e
Adriana Rose <adriana.rose.dh@gmail.com>
2025-05-19 19:17:37 -0700

    lets see if this returns the one

--- components/forms/reservation/ReservationModal.tsx
+++ components/forms/reservation/ReservationModal.tsx
@@ -174,20 +174,11 @@ export default function ReservationModal({ lang }: ReservationModalProps) {
                   {/* Tock Widget container */}
                   <div className="flex-1 flex justify-center items-start p-6">
                     <div className="p-6 bg-white rounded-xl w-full max-w-lg">
-                      <div
-                        id="Tock_widget_container"
-                        data-tock-display-mode="Inline"
-                        data-tock-widget="data-tock-offering"
-                        data-tock-offering-id="528232"
-                        data-tock-color-mode="White"
-                        data-tock-locale="es-mx"
-                        data-tock-timezone="America/Tijuana"
-                        style={{ width: "100%", minHeight: "400px" }}
-                      />
+                      {reservationType === "restaurant" && <TockWidget />}
                     </div>
                   </div>
                 
-                  {reservationType === "restaurant" && <TockWidget />}
+                  
                 </div>
 
                 {/* Café Pane */}
