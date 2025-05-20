b46ef8627a34c3040b970c7ccd49560769eb8af2
Adriana Rose <adriana.rose.dh@gmail.com>
2025-05-19 19:34:08 -0700

    returned init to tock script

--- app/layout.tsx
+++ app/layout.tsx
@@ -29,22 +29,16 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
 
          {/* Tock Stub WITHOUT init */}
         
-         <Script id="tock-script" strategy="beforeInteractive">
+        <Script id="tock-script" strategy="beforeInteractive">
           {`
-          !function(t,o,c,k){
-            if(!t.tock){
-              var e=t.tock=function(){
-                e.callMethod? e.callMethod.apply(e,arguments):e.queue.push(arguments)
-              };
-              t._tock||(t._tock=e),e.push=e,e.loaded=!0,e.version='1.0',e.queue=[];
-              var f=o.createElement(c);f.async=!0,f.src=k;
-              var g=o.getElementsByTagName(c)[0];g.parentNode.insertBefore(f,g);
-            }
-          }(window,document,'script','https://www.exploretock.com/tock.js');
+            !function(t,o,c,k){if(!t.tock){var e=t.tock=function(){e.callMethod?
+            e.callMethod.apply(e,arguments):e.queue.push(arguments)};t._tock||(t._tock=e),
+            e.push=e,e.loaded=!0,e.version='1.0',e.queue=[];var f=o.createElement(c);f.async=!0,
+            f.src=k;var g=o.getElementsByTagName(c)[0];g.parentNode.insertBefore(f,g)}}(
+            window,document,'script','https://www.exploretock.com/tock.js');
+            tock('init', 'olivea-farm-to-table');
           `}
-
         </Script>
-
 
       </head>
 
