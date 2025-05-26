0f594de2a8095d04c0ca1225ea2d0c44691bd6a1
Adriana Rose <adriana.rose.dh@gmail.com>
2025-05-25 20:43:59 -0700

    removing conditional from transition video

--- app/providers.tsx
+++ app/providers.tsx
@@ -3,7 +3,6 @@
 
 import { ReactNode } from "react";
 import dynamic from "next/dynamic";
-import { usePathname } from "next/navigation";
 import { SharedTransitionProvider } from "@/contexts/SharedTransitionContext";
 import { ReservationProvider } from "@/contexts/ReservationContext";
 import { ScrollProvider } from "@/components/providers/ScrollProvider";
@@ -21,24 +20,18 @@ const ReservationModal = dynamic(
 );
 
 export function AppProviders({ children }: AppProvidersProps) {
-  const pathname = usePathname();
-  // Home paths: "/", "/en", or "/es"
-  const isHome = pathname === "/" || /^\/(en|es)\/?$/.test(pathname);
-
   return (
     <SharedTransitionProvider>
       <ReservationProvider>
         <ScrollProvider>
           <ClientProviders>
             {children}
-            {/* Global reservation modal */}
             <ReservationModal lang="es" />
           </ClientProviders>
         </ScrollProvider>
       </ReservationProvider>
 
-      {/* Only show the shared-element video transition on non-home pages */}
-      {!isHome && <SharedVideoTransition />}
+      <SharedVideoTransition /> {/* Always present */}
     </SharedTransitionProvider>
   );
 }
