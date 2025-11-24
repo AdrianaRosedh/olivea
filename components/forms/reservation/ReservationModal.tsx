3463211658fcfb597aa3b0a952f0043277317155
Adriana Rose <adriana.rose.dh@gmail.com>
2025-11-24 12:17:15 -0800

    attempt to fix scroll issue

--- components/forms/reservation/ReservationModal.tsx
+++ components/forms/reservation/ReservationModal.tsx
@@ -208,7 +208,7 @@ export default function ReservationModal({ lang }: ReservationModalProps) {
                   <CloudbedsWidget autoLaunch={isOpen} />
                 </div>
               )}
-            </div>          
+            </div>
 
             {/* RESTAURANT */}
             <div
