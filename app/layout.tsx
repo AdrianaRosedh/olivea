798ef67972773ef7390ffa4cfafcd1f53c9c90b5
Adriana Rose <adriana.rose.dh@gmail.com>
2026-02-19 15:49:18 -0800

    updating manifest errors

--- app/layout.tsx
+++ app/layout.tsx
@@ -74,7 +74,7 @@ export const metadata: Metadata = {
     apple: [{ url: "/apple-icon.png", sizes: "180x180" }],
   },
 
-  manifest: "/manifest.json",
+  manifest: "/manifest.webmanifest",
 };
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
