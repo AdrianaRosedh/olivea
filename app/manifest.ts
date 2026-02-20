db3db9df2a3856e76f0cddad91a5f6380506e374
Adriana Rose <adriana.rose.dh@gmail.com>
2026-02-19 18:21:28 -0800

    Bust icon cache: v2 icons

--- app/manifest.ts
+++ app/manifest.ts
@@ -11,8 +11,8 @@ export default function manifest(): MetadataRoute.Manifest {
     background_color: "#e9e8e5",
     theme_color: "#65735b",
     icons: [
-      { src: "/icons/icon-192.png", sizes: "192x192", type: "image/png", purpose: "any" },
-      { src: "/icons/icon-512.png", sizes: "512x512", type: "image/png", purpose: "any" }
+      { src: "/icons/icon-192-v2.png", sizes: "192x192", type: "image/png", purpose: "any" },
+      { src: "/icons/icon-512-v2.png", sizes: "512x512", type: "image/png", purpose: "any" }
     ],
   };
 }
