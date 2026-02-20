3ec109038e095f2724d26907eb4bc55eaca3a9fd
Adriana Rose <adriana.rose.dh@gmail.com>
2026-02-19 16:08:08 -0800

    attempt to fix analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -90,7 +90,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
             window.dataLayer = window.dataLayer || [];
             function gtag(){dataLayer.push(arguments);}
             gtag('js', new Date());
-            gtag('config', 'G-M3JEDWZ732');
+            gtag('config', 'G-M3JEDWZZ732');
           `}
         </Script>
       </head>
