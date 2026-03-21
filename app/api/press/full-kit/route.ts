b96ef742169a3bc8a15bf928d8449393129dbe4a
Adriana Rose <adriana.rose.dh@gmail.com>
2026-03-21 09:43:53 -0700

    improved pres kit download

--- app/api/press/full-kit/route.ts
+++ app/api/press/full-kit/route.ts
@@ -1,7 +1,7 @@
 import { NextResponse } from "next/server";
 
 const DRIVE_DIRECT =
-  "https://drive.google.com/file/d/1QyRiSUbnxoOzqNcR5ou6VvZA6yFSu0V0/view?usp=sharing";
+  "https://drive.google.com/uc?export=download&id=1QyRiSUbnxoOzqNcR5ou6VvZA6yFSu0V0";
 
 export function GET() {
   return NextResponse.redirect(DRIVE_DIRECT);
